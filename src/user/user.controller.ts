d67e6c884d85ab5fb070c2c7962472718fe87be5
DesenvTech <claudineiarneckedelima@gmail.com>
2022-02-21 23:29:01 -0300

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { AuthRequest } from 'src/auth/interfaces/AuthRequest';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @IsPublic()
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findOneByEmail(@Request() req: AuthRequest) {
+    return this.userService.findOneByEmail(req.user.email);
+  }
+}
